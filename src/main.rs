b994adbc9e86ab4db646192d5b24818150888365
Evan <evan@spectrum.md>
2022-09-03 18:12:09 -0700

    Initial commit for game

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+use bevy::{
+    prelude::*,
+    render::camera::{DepthCalculation, ScalingMode, WindowOrigin},
+};
+
+pub const CLEAR: Color = Color::rgb(0.1, 0.1, 0.1);
+pub const RESOLUTION: f32 = 16.0 / 9.0;
+fn main() {
+    let height: f32 = 900.0;
+    App::new()
+        .insert_resource(ClearColor(CLEAR))
+        .insert_resource(WindowDescriptor {
+            width: height * RESOLUTION,
+            height: height,
+            title: "Chat Platformer".to_string(),
+            resizable: false,
+            ..Default::default()
+        })
+        .add_startup_system(spawn_camera)
+        .add_startup_system(spawn_player)
+        .add_plugins(DefaultPlugins)
+        .run();
+}
+
+fn spawn_camera(mut commands: Commands) {
+    let mut camera = Camera2dBundle::default();
+
+    let projection: OrthographicProjection = OrthographicProjection {
+        top: 1.0,
+        bottom: -1.0,
+        right: 1.0 * RESOLUTION,
+        left: -1.0 * RESOLUTION,
+        near: 1.0,
+        far: -1.0,
+        scaling_mode: ScalingMode::None,
+        window_origin: WindowOrigin::Center,
+        scale: 1.0,
+        depth_calculation: DepthCalculation::Distance,
+    };
+
+    camera.projection = projection;
+
+    commands.spawn_bundle(camera);
+}
+
+fn spawn_player(mut commands: Commands) {}

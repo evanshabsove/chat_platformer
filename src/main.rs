01eaa645f933f442472c2a5a249218be375c15b4
Evan <evan@spectrum.md>
2022-09-06 14:45:44 -0700

    Grey background on sprite

--- src/main.rs
+++ src/main.rs
@@ -42,7 +42,7 @@ fn spawn_player(mut commands: Commands, ascii: Res<AsciiSheet>, asset_server: Re
     sprite.color = Color::rgb(0.3, 0.3, 0.9);
     sprite.custom_size = Some(Vec2::splat(100.0));
 
-    commands
+    let player = commands
         .spawn_bundle(SpriteSheetBundle {
             sprite: sprite,
             texture_atlas: ascii.0.clone(),
@@ -52,7 +52,27 @@ fn spawn_player(mut commands: Commands, ascii: Res<AsciiSheet>, asset_server: Re
             },
             ..Default::default()
         })
-        .insert(Name::new("Player"));
+        .insert(Name::new("Player"))
+        .id();
+
+    let mut sprite = TextureAtlasSprite::new(0);
+    sprite.color = Color::rgb(0.5, 0.5, 0.5);
+    sprite.custom_size = Some(Vec2::splat(100.0));
+
+    let background = commands
+        .spawn_bundle(SpriteSheetBundle {
+            sprite: sprite,
+            texture_atlas: ascii.0.clone(),
+            transform: Transform {
+                translation: Vec3::new(0.0, 0.0, -1.0),
+                ..Default::default()
+            },
+            ..Default::default()
+        })
+        .insert(Name::new("Background"))
+        .id();
+
+    commands.entity(player).push_children(&[background]);
 }
 
 struct AsciiSheet(Handle<TextureAtlas>);

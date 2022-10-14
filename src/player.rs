00a32b6a2bde49faeddd5343a8420b3c578f20d8
Evan <evan@spectrum.md>
2022-10-14 13:18:03 -0700

    Making sprite animate

--- src/player.rs
+++ src/player.rs
@@ -12,9 +12,13 @@ pub struct Player {}
 impl Plugin for PlayerPugin {
     fn build(&self, app: &mut App) {
         app.add_startup_system(spawn_player)
-            .add_system(camera_movement);
+            .add_system(camera_movement)
+            .add_system(animate_sprite);
     }
 }
+
+#[derive(Component, Deref, DerefMut)]
+struct AnimationTimer(Timer);
 
 fn camera_movement(
     player_query: Query<&Transform, With<Player>>,
@@ -33,7 +37,15 @@ fn spawn_player(
     mut texture_atlases: ResMut<Assets<TextureAtlas>>,
 ) {
     let texture_handle = asset_server.load("player/Character_004.png");
-    let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(16.0, 30.0), 3, 4);
+    let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(24.0, 24.0), 3, 4);
+    // let texture_atlas = TextureAtlas::from_grid_with_padding(
+    //     texture_handle,
+    //     Vec2::splat(16.0),
+    //     3,
+    //     4,
+    //     Vec2::splat(16.0),
+    //     Vec2::splat(16.0),
+    // );
     let texture_atlas_handle = texture_atlases.add(texture_atlas);
     commands
         .spawn_bundle(SpriteSheetBundle {
@@ -56,12 +68,31 @@ fn spawn_player(
             force: Vec2::new(0.0, 0.0),
             torque: 0.0,
         })
-        .insert(Collider::cuboid(TILE_SIZE / 2.0, TILE_SIZE / 2.0))
+        .insert(Collider::cuboid(TILE_SIZE / 2.0, (TILE_SIZE / 2.0) + 4.0))
         .insert(GravityScale(GRAV))
         .insert(ActiveEvents::COLLISION_EVENTS)
         // .insert(LockedAxes::ROTATION_LOCKED)
         .insert(Mover {
             speed: 200.0,
             is_jumping: false,
-        });
+        })
+        .insert(AnimationTimer(Timer::from_seconds(0.1, true)));
+}
+
+fn animate_sprite(
+    time: Res<Time>,
+    texture_atlases: Res<Assets<TextureAtlas>>,
+    mut query: Query<(
+        &mut AnimationTimer,
+        &mut TextureAtlasSprite,
+        &Handle<TextureAtlas>,
+    )>,
+) {
+    for (mut timer, mut sprite, texture_atlas_handle) in &mut query {
+        timer.tick(time.delta());
+        if timer.just_finished() {
+            let texture_atlas = texture_atlases.get(texture_atlas_handle).unwrap();
+            sprite.index = (sprite.index + 1) % texture_atlas.textures.len();
+        }
+    }
 }
